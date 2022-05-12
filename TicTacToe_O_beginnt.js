ab24a91ca165584e7b852835806b58ff1ef58c22
oliw132 <98960512+oliw132@users.noreply.github.com>
2022-05-12 15:04:51 +0200

    Add files via upload

--- TicTacToe_O_beginnt.js
+++ TicTacToe_O_beginnt.js
@@ -1,4 +1,4 @@
-var Player = "X";
+var Player = "O";
 var wX=0;
 var wO=0;
 
@@ -7,14 +7,32 @@ var wO=0;
 	if (document.getElementById(lID).innerHTML.trim() != "") {
 		return;
 	}
-	if (Player == "X") {
+	if (Player == "O") {
+		document.getElementById(lID).innerHTML = "O";
+		wO=wO | ( 2 ** (lID - 1 ) );
+		console.log ( wO );
+		console.log ( wO.toString (2) );
+		console.log ( 2 ** (lID - 1 ) );
+		Player = "X";
+		
+			if ( ( wO & 7) == 7 ) { alert("O Won");location.reload()};
+			if ( ( wO & 56) == 56 ) { alert("O Won");location.reload()};
+			if ( ( wO & 448) == 448 ) { alert("O Won");location.reload()};
+			if ( ( wO & 73) == 73 ) { alert("O Won");location.reload()};
+			if ( ( wO & 146) == 146 ) { alert("O Won");location.reload()};
+			if ( ( wO & 292) == 292 ) { alert("O Won");location.reload()};
+			if ( ( wO & 273) == 273 ) { alert("O Won");location.reload()};
+			if ( ( wO & 84) == 84 ) { alert("O Won");location.reload()};
+			
+	} 	
+	
+	else {
 		document.getElementById(lID).innerHTML = "X";
 		wX=wX | ( 2 ** (lID - 1 ) );
 		console.log ( wX );
 		console.log ( wX.toString (2) );
 		console.log ( 2 ** (lID - 1 ) );
 		Player = "O";
-		
 			if ( ( wX & 7) == 7 ) { alert("X Won");location.reload()};
 			if ( ( wX & 56) == 56 ) { alert("X Won");location.reload()};
 			if ( ( wX & 448) == 448 ) { alert("X Won");location.reload()};
@@ -39,26 +57,6 @@ var wO=0;
 			if ( ( wX & 241) == 241 ) { alert("Draw");location.reload()};
 			if ( ( wX & 286) == 286 ) { alert("Draw");location.reload()};
 			if ( ( wX & 115) == 115 ) { alert("Draw");location.reload()};
-			
-			
-	} 	
-	
-	else {
-		document.getElementById(lID).innerHTML = "O";
-		wO=wO | ( 2 ** (lID - 1 ) );
-		console.log ( wO );
-		console.log ( wO.toString (2) );
-		console.log ( 2 ** (lID - 1 ) );
-		Player = "X";
-		
-			if ( ( wO & 7) == 7 ) { alert("O Won");location.reload()};
-			if ( ( wO & 56) == 56 ) { alert("O Won");location.reload()};
-			if ( ( wO & 448) == 448 ) { alert("O Won");location.reload()};
-			if ( ( wO & 73) == 73 ) { alert("O Won");location.reload()};
-			if ( ( wO & 146) == 146 ) { alert("O Won");location.reload()};
-			if ( ( wO & 292) == 292 ) { alert("O Won");location.reload()};
-			if ( ( wO & 273) == 273 ) { alert("O Won");location.reload()};
-			if ( ( wO & 84) == 84 ) { alert("O Won");location.reload()};
 	}
 
 }
